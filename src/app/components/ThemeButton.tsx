ad6bf67136d1f5751e4aea7fd802c292891f429d
hyperexe805 <sendmemes36@gmail.com>
2024-12-30 23:13:09 +0100

    ruleoff

--- src/app/components/ThemeButton.tsx
+++ src/app/components/ThemeButton.tsx
@@ -7,10 +7,14 @@ const ThemeButton = () => {
   // Usar useEffect para establecer el tema inicial cuando el componente se monta
   useEffect(() => {
     // Verifica el tema preferido del usuario al cargar la página
-    const savedTheme = localStorage.getItem('theme') || 'light'; // Si no hay valor en localStorage, usar 'light' como predeterminado
-    setTheme(savedTheme as 'dark' | 'light');
+    const savedTheme = localStorage.getItem('theme') as 'dark' | 'light' || 'light';
+    setTheme(savedTheme);
     // Aplica el tema al cargar el componente
-    document.body.classList.toggle('dark', savedTheme === 'dark');
+    if (savedTheme === 'dark') {
+      document.body.classList.add('dark');
+    } else {
+      document.body.classList.remove('dark');
+    }
   }, []);
 
   // Cambiar el tema
@@ -20,16 +24,19 @@ const ThemeButton = () => {
     localStorage.setItem('theme', newTheme); // Guarda la preferencia del tema en localStorage
 
     // Alterna la clase en el body para cambiar el tema
-    document.body.classList.toggle('dark', newTheme === 'dark');
+    if (newTheme === 'dark') {
+      document.body.classList.add('dark');
+    } else {
+      document.body.classList.remove('dark');
+    }
   };
 
   return (
     <button
       onClick={toggleTheme}
       className="p-4 flex justify-center items-center"
-
     >
-      {theme === 'dark' ? <Sun /> : <Moon  />}
+      {theme === 'dark' ? <Sun /> : <Moon />}
     </button>
   );
 };
